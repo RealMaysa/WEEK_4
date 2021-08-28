13cde86806a7288817701197bd435143e409569c
Pimpraethong Rotpotchanarat <86197283+RealMaysa@users.noreply.github.com>
2021-08-28 19:32:14 +0700

    Update LAB_teast.c

--- LAB_teast.c
+++ LAB_teast.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int a,b,c,s,pt;
+    int a,b,c,s;
+    double pt;
 printf("Enter a first side: ");
 scanf("%d",&a);
 printf("Enter a second side: ");
@@ -12,9 +13,9 @@ if(a+b>c && a+c>b && b+c>a){
 printf("%d %d %d\n",a,b,c);
 s=(a+b+c)/2;
 printf("%d %d %d %d\n",s,a,b,c);
-pt=pow(s*(s-a)*(s-b)*(s-c),0.5);
+pt=pow(s*(s-a)*(s-b)*(s-c),1.0f/2.0f);
 printf("02 %d %d %d %d\n",s,a,b,c);
-printf("%d",pt);
+printf("%f",pt);
 }
 else{printf("%d %d %d\n",a,b,c);
     printf("0");
